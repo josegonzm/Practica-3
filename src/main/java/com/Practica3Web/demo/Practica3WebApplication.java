77599f92491d2c12e2e63f29ce2dbe6c1ab53c05
josegonzm <jgonzalez50464@ufide.ac.cr>
2023-06-25 11:29:37 -0600

    Template

---
+++ src/main/java/com/Practica3Web/demo/Practica3WebApplication.java
@@ -0,0 +1,13 @@
+package com.Practica3Web.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica3WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica3WebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Practica3Web/demo/Practica3WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Practica3Web.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica3WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
